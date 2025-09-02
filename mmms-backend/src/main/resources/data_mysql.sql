828e159744f930d4ed52ec93e7fa421069b6ed8d
kushvlshresthv <kushvlshresthv@gmail.com>
2025-09-02 11:49:03 +0545

    updated table for deployment

--- mmms-backend/src/main/resources/data_mysql.sql
+++ mmms-backend/src/main/resources/data_mysql.sql
@@ -1,11 +1,2 @@
 INSERT IGNORE INTO app_users (username, password, email, firstname, lastname)
 VALUES ('username', '{noop}password', 'username@gmail.com', 'admin', 'admin');
-
-INSERT IGNORE INTO app_users (username, password, email, firstname, lastname)
-VALUES ('newUsername', '{noop}password', 'username@gmail.com', 'admin', 'admin');
-
-# TODO: alter table query, will fix later to modify ta create table statement itself
-
-ALTER TABLE committees CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE meetings CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE committee_memberships CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
